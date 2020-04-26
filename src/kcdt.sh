015ee205b133233d82f677453d3924d641e8f7fb
Anton Whalley <anton@venshare.com>
2020-04-26 15:35:48 +0100

    remove new line

--- src/kcdt.sh
+++ src/kcdt.sh
@@ -56,7 +56,7 @@ UUID=$(uuidgen)
 
 DUMP_NAME="${UUID}-dump-${TS}-${HOSTNAME}-${EXE_NAME}-${REAL_PID}-${SIGNAL}"
 
-echo "{ \"uuid\":\"${UUID}\", \"dump_file\":\"${DUMP_NAME}.core${EXT}\", \"ext\": \"${EXT}\", \"timestamp\": \"${TS}\", \"hostname\": \"${HOSTNAME}\", \"exe\": \"${EXE_NAME}\", \"real_pid\": \"${REAL_PID}\", \"signal\": \"${SIGNAL}\" }\n" > "${DIRECTORY}/${DUMP_NAME}-dump-info.json" 
+echo "{ \"uuid\":\"${UUID}\", \"dump_file\":\"${DUMP_NAME}.core${EXT}\", \"ext\": \"${EXT}\", \"timestamp\": \"${TS}\", \"hostname\": \"${HOSTNAME}\", \"exe\": \"${EXE_NAME}\", \"real_pid\": \"${REAL_PID}\", \"signal\": \"${SIGNAL}\" }" > "${DIRECTORY}/${DUMP_NAME}-dump-info.json" 
 
 head --bytes "${LIMIT_SIZE}" | tee ${DUMP_NAME}.core | (${COMPRESSOR} > "${DIRECTORY}/${DUMP_NAME}.core${EXT}")
 
